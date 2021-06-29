fa981c760aaa7584420269cd70a506e85601cb58
milan2798 <zadfiyamilan112@gmail.com>
2021-06-29 22:58:23 +0530

    fixed dark-mode

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const options = {
   right: 'unset', // default: '32px'
   left: '32px', // default: 'unset'
   time: '0.5s', // default: '0.3s'
-  mixColor: '#e8e9d9', // default: '#fff'
+  mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
